74878dde834bae0baf3680aea31953d0697550f4
CuongNguyen <chrollolucifer97@gmail.com>
2024-03-24 20:31:58 +0700

    fix lại phần tìm kiếm

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        if (window.scrollY) {
+            document.getElementById('back-top').style.opacity = '1'; // Hiển thị nút
+        } else {
+            document.getElementById('back-top').style.opacity = '0'; // Ẩn nút
+        }
+    });
+
+    document.getElementById('back-top').addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
